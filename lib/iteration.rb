d84f6dc9291bbe63396023280b249817431e683e
Erin Reames <italiagirl83@gmail.com>
2020-03-26 19:30:49 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -64,6 +64,6 @@ def total_even_pairs(src)
   
   if src[counter][0] % 2 == 0 && src[counter][1] % 2 == 0 
     
-    total = total += (src [counter])
+    total = total += (src [counter].sum)
   
 end
